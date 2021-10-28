99fef5da0e36d75d6f890c2f28827c111bbed2de
alphashaw <shawalpha5@gmail.com>
2021-10-28 02:15:00 +0000

    refactored,...

--- deployments/docker/bookkeeper/scripts/stop.sh
+++ deployments/docker/bookkeeper/scripts/stop.sh
@@ -21,8 +21,12 @@ set -e -x
 source /scripts/common.sh >/dev/null
 
 function killBookie() {
-  printf "Stopping the bookie in the background"
+  printf "Stopping the bookie"
   lsof -i :"$BK_PORT" | grep LISTEN | awk '{print $2}' | xargs kill 2>/dev/null
+  echo "Eager kill the process keeping the docker runtime instead of waiting for kubernetes 'TerminationGracePeriodSeconds'"
+  SLEEP_PROCESS=$(cat sleep.pid)
+  echo "killing the processing = $SLEEP_PROCESS"
+  kill "$SLEEP_PROCESS"
 }
 
 function decommissionBookie() {
@@ -47,6 +51,7 @@ function decommissionBookie() {
 
 if [ ! -f bookie_started ]; then
     echo "The bookie was never ready, bookie_started file missing"
+    killBookie
     return
 fi
 
@@ -55,11 +60,3 @@ rm bookie_started ## remove the start indication file
 decommissionBookie
 
 killBookie
-
-echo "Eager kill the process keeping the docker runtime instead of waiting for kubernetes 'TerminationGracePeriodSeconds'"
-
-SLEEP_PROCESS=$(cat sleep.pid)
-
-echo "killing the processing = $SLEEP_PROCESS"
-
-kill "$SLEEP_PROCESS"
